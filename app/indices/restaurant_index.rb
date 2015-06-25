87f0ded8fd32d0645ba42f78e1e663a69948213b
Rolando <rehgarza@gmail.com>
2015-06-25 16:47:55 -0500

    Comente el contenido de los idices para sphinx restaurants_index.rb

--- app/indices/restaurant_index.rb
+++ app/indices/restaurant_index.rb
@@ -1,9 +1,9 @@
-ThinkingSphinx::Index.define :restaurant, :with => :active_record do
-  # fields
-  indexes :name, :description
-  indexes dishs(:name)
-
-  # attributes
-
-  has created_at, updated_at
-end
+# ThinkingSphinx::Index.define :restaurant, :with => :active_record do
+#   # fields
+#   indexes :name, :description
+#   indexes dishs(:name)
+#
+#   # attributes
+#
+#   has created_at, updated_at
+# end
