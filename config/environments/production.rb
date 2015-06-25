deb9a0aa509344579cb8a715f7e80e2d39427cd4
Rolando <rehgarza@gmail.com>
2015-06-25 18:30:09 -0500

    host change to heroku app on production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = {:host => 'localhost'}
+  config.action_mailer.default_url_options = {:host => 'https://damp-wave-8445.herokuapp.com/'}
   config.action_mailer.smtp_settings = {
       :address => "smtp.gmail.com",
       :port => 587,
